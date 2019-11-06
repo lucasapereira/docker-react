1cc4ecf2eea5bbaa4499b032f58b6f34178f140a
92861563168 <lucas.araujo.pereira@gmail.com>
2019-11-06 10:18:53 -0300

    mudanca

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        EAe nogas
+        EAe nogas Da uma olhada
         </p>
  
         <a
